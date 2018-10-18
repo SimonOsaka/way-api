3481e70b631ac0ce0def117e55f52650369e7830
许忠亮 <geniusmickymouse@qq.com>
2018-10-18 19:24:08 +0800

    优化ehcache

--- src/main/java/com/zl/way/city/mapper/WayCityMapper.java
+++ src/main/java/com/zl/way/city/mapper/WayCityMapper.java
@@ -1,6 +1,7 @@
 package com.zl.way.city.mapper;
 
 import com.zl.way.city.model.WayCity;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -8,20 +9,15 @@ public interface WayCityMapper {
 
     int deleteByPrimaryKey(Integer id);
 
-
     int insert(WayCity record);
-
 
     int insertSelective(WayCity record);
 
-
     WayCity selectByPrimaryKey(Integer id);
-
 
     int updateByPrimaryKeySelective(WayCity record);
 
-
     int updateByPrimaryKey(WayCity record);
 
-    List<WayCity> selectAllByCondition(Byte isUsed);
+    List<WayCity> selectAllByCondition(@Param("isUsed") Byte isUsed);
 }
