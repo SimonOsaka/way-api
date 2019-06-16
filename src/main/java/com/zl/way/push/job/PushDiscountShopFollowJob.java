ce2a022be7f742a5c8820d05a733c5dfc430b163
许忠亮 <geniusmickymouse@qq.com>
2019-06-16 18:28:07 +0800

    [PUSH] 定时任务-修改推送商家优惠的时间

--- src/main/java/com/zl/way/push/job/PushDiscountShopFollowJob.java
+++ src/main/java/com/zl/way/push/job/PushDiscountShopFollowJob.java
@@ -1,12 +1,13 @@
 package com.zl.way.push.job;
 
-import com.zl.way.push.service.WayDiscountJpushService;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.zl.way.push.service.WayDiscountJpushService;
 
 @Component
 public class PushDiscountShopFollowJob {
@@ -19,7 +20,7 @@ public class PushDiscountShopFollowJob {
         this.discountJpushService = discountJpushService;
     }
 
-    @Scheduled(fixedDelay = 3600000 * 6)
+    @Scheduled(cron = "0 30 10,16,19 * * ?")
     public void doJob() {
         logger.info("开始做推送任务，时间：{}", DateTime.now().toDate());
         discountJpushService.discountPush();
